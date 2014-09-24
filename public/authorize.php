43ead0fc75385db80c7edacf04492d833db1398c
Jakob Buis <jpabuis@gmail.com>
2014-09-24 18:16:10 +0200

    added authorization warning

--- public/authorize.php
+++ public/authorize.php
@@ -4,10 +4,14 @@
         <title>Bolklogin</title>
         <style>
             .error { border-radius: 0.25em; padding: 1em; background-color: red; color: white; }
+            .warning { border-radius: 0.25em; padding: 1em; background-color: #ffffd5; color: black; }
         </style>
     </head>
     <body>
         <h1>Bolklogin</h1>
+        <p class="warning">
+            <strong>Let op:</strong> <?= $_GET['client_id']; ?> krijgt toegang tot al jouw gegevens in de ledenadminstratie.
+        </p>
         <form action="<?= $_SERVER['REQUEST_URI'];?>" method="POST">
             <p>
                 <label for="authorization">Ik geef toestemming</label>
