7ec1a136ddca9db2ca3a8748eee4506548fd6a67
Jakob Buis <jpabuis@gmail.com>
2015-01-12 18:16:32 +0100

    added options code

--- src/Token.php
+++ src/Token.php
@@ -5,6 +5,20 @@
 class Token extends Tonic\Resource {
 
     /**
+     * @method OPTIONS
+     */
+    function options()
+    {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET');
+        header('Access-Control-Allow-Headers: Authorization');
+        header('Access-Control-Max-Age: 1');  //1728000
+        header("Content-Length: 0");
+        header("Content-Type: text/plain charset=UTF-8");
+        exit(0);
+    }
+
+    /**
      * @method POST
      * @method GET
      */
