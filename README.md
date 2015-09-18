e176e9424cca07d2cc4ecb2ee9425f758468b349
Jakob Buis <jakob@cubiso.nl>
2015-09-18 17:06:01 +0200

    added database structure and instructions

--- README.md
+++ README.md
@@ -3,11 +3,12 @@ OAuth2 authorization server for Bolk-accounts
 
 ## Deployment
 1. Install on a VPS.
-1. Ensure Apache 2, PHP5 and MySQL are present. 
+1. Ensure Apache 2, PHP5 and MySQL are present.
+1. Create database and create the tables from database.sql.
 1. Copy `web/.htaccess.example` to `web/.htaccess` and fill in the details
 1. Install php5-ldap
 1. Install dependencies by running `composer install`
 1. Point webroot of apache to `web`
 
 ## License
-Unknown. For now, assume all rights reserved 2012-2014 Jakob Buis and Max Maton. 
+Unknown. For now, assume all rights reserved 2012-2014 Jakob Buis and Max Maton.
---
+++ database.sql
@@ -0,0 +1,44 @@
+CREATE TABLE IF NOT EXISTS `oauth_access_tokens` (
+  `access_token` varchar(40) NOT NULL,
+  `client_id` varchar(80) NOT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `scope` varchar(2000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `oauth_authorization_codes` (
+  `authorization_code` varchar(40) NOT NULL,
+  `client_id` varchar(80) NOT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `redirect_uri` varchar(2000) DEFAULT NULL,
+  `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `scope` varchar(2000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `oauth_clients` (
+  `client_id` varchar(80) NOT NULL,
+  `client_secret` varchar(80) NOT NULL,
+  `redirect_uri` varchar(2000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `oauth_refresh_tokens` (
+  `refresh_token` varchar(40) NOT NULL,
+  `client_id` varchar(80) NOT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `scope` varchar(2000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `oauth_scopes` (
+  `scope` text,
+  `is_default` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `oauth_access_tokens`
+ ADD PRIMARY KEY (`access_token`);
+ALTER TABLE `oauth_authorization_codes`
+ ADD PRIMARY KEY (`authorization_code`);
+ALTER TABLE `oauth_clients`
+ ADD PRIMARY KEY (`client_id`);
+ALTER TABLE `oauth_refresh_tokens`
+ ADD PRIMARY KEY (`refresh_token`);
