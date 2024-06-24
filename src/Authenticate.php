9d2729d2df87f6becb616a8121fc3468b20d3a43
Alchemi1963 <alchemi1999@hotmail.com>
2024-06-24 18:58:18 +0200

    Troubleshooting Authenticate.php

--- src/Authenticate.php
+++ src/Authenticate.php
@@ -30,9 +30,9 @@ class Authenticate extends Tonic\Resource
 
         // Show an error if the request is invalid
         if (!$this->app->server->validateAuthorizeRequest($request, $response)) {
-            $error = $response.getParameter('error');
-            $error_description = $response.getParameter('error_description');
-            return $this->displayError('Parameters are invalid');
+            //$error = $response.getParameter('error');
+            //$error_description = $response.getParameter('error_description');
+            return $this->displayError("Parameters are invalid {get_class($response}");
         }
 
         /*
